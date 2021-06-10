677f572ce9411b5f650e4b0c654720c69309c8f0
aashish-07 <iaashish07@gmail.com>
2021-06-10 18:49:08 +0530

    advance

---
+++ Dynamic Programming/longest_inc_subsequence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int dp[n];
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[i])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+    int lis = 0;
+    for (int i = 0; i < n; i++)
+    {
+        lis = max(lis, dp[i]);
+    }
+    cout << lis;
+}
